d3280ebac0e0ff259f330ba78e3b351d79fe9d15
Brian <brians@terpmail.umd.edu>
2024-04-22 22:02:35 -0400

    Adding Player 2's turn

--- TupleOut.py
+++ TupleOut.py
@@ -6,7 +6,8 @@ p2 = 0
 dice_face = (1, 2, 3, 4, 5, 6)
 
 #Code for game 
-while p1 < 50:
+for index in range(5):
+    #Player 1
     start = input("Player 1, press 'ENTER' to start your turn:")
 
     #Generates dice roll for player 1
@@ -57,4 +58,54 @@ while p1 < 50:
     p1 += p1d1 + p1d2 + p1d3
     print(f"Player 1's score is {p1} \n")
     
+    #Player 2
+    start = input("Player 2, press 'ENTER' to start your turn:")
+
+    #Generates dice roll for player 2
+    p2d1 = random.choice(dice_face)
+    p2d2 = random.choice(dice_face)
+    p2d3 = random.choice(dice_face)
+    print(f"Your dice roll is: {p2d1}, {p2d2}, {p2d3}")
+
+    #Checks for tuple out
+    if p2d1 == p2d2 and p2d1 == p2d3:
+        print("You have Tupled Out! You lose.")
+        break
+    
+    #Checks fir fixed dice and option to re-roll
+    while p2d1 == p2d2 or p2d1 == p2d3 or p2d2 == p2d3:
+        if p2d1 == p2d2:
+            roll = input("Do you want to re-roll your 3rd dice? (Y/N) \n")
+            if roll.upper() == 'Y':
+                p2d3 = random.choice(dice_face)
+                print(f"Your dice roll is: {p2d1}, {p2d2}, {p2d3} \n")
+                if p2d1 == p2d2 and p2d1 == p2d3:
+                    print("You have Tupled Out! You lose.")
+                    break
+            else:
+                break
+        if p2d1 == p2d3:
+            roll = input("Do you want to re-roll your 2nd dice? (Y/N) \n")
+            if roll.upper() == 'Y':
+                p2d2 = random.choice(dice_face)
+                print(f"Your dice roll is: {p2d1}, {p2d2}, {p2d3} \n")
+                if p2d1 == p2d2 and p2d1 == p2d3:
+                    print("You have Tupled Out! You lose.")
+                    break
+            else:
+                break
+        if p2d2 == p2d3:
+            roll = input("Do you want to re-roll your 1st dice? (Y/N) \n")
+            if roll.upper() == 'Y':
+                p2d1 = random.choice(dice_face)
+                print(f"Your dice roll is: {p2d1}, {p2d2}, {p2d3} \n")
+                if p2d1 == p2d2 and p2d1 == p2d3:
+                    print("You have Tupled Out! You lose.")
+                    break
+            else:
+                break
+
+    #Displaying score for P2
+    p2 += p2d1 + p2d2 + p2d3
+    print(f"Player 2's score is {p2} \n")
 
