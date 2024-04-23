ba63b9f9247b3b3f31d855c9a78dc1d2b9fa9c3e
Brian <brians@terpmail.umd.edu>
2024-04-22 21:33:33 -0400

    Pasting re-roll for all dice positions

--- TupleOut.py
+++ TupleOut.py
@@ -23,10 +23,25 @@ while p1 < 50:
     #Checks fir fixed dice and option to re-roll
     if p1d1 == p1d2 or p1d1 == p1d3 or p1d2 == p1d3:
         while p1d1 == p1d2:
-            roll = input("Do you want to re-roll your 3rd dice? (Y/N) \n")
+            roll = input("Do you want to re-roll your 3rd dice? (Y/N) ")
             if roll.upper() == 'Y':
                 p1d3 = random.choice(dice_face)
                 print(f"Your dice roll is: {p1d1}, {p1d2}, {p1d3} \n")
             else:
                 break
+        while p1d1 == p1d3:
+            roll = input("Do you want to re-roll your 2nd dice? (Y/N) ")
+            if roll.upper() == 'Y':
+                p1d2 = random.choice(dice_face)
+                print(f"Your dice roll is: {p1d1}, {p1d2}, {p1d3} \n")
+            else:
+                break
+        while p1d2 == p1d3:
+            roll = input("Do you want to re-roll your 1st dice? (Y/N) ")
+            if roll.upper() == 'Y':
+                p1d1 = random.choice(dice_face)
+                print(f"Your dice roll is: {p1d1}, {p1d2}, {p1d3} \n")
+            else:
+                break
+    
 
