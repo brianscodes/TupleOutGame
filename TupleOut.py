c6a5a94355e6f3e51eb50895b1a5f90e4fd73cb0
Brian <brians@terpmail.umd.edu>
2024-04-22 21:29:55 -0400

    Testing fixed dice and re-roll

--- TupleOut.py
+++ TupleOut.py
@@ -7,16 +7,26 @@ dice_face = (1, 2, 3, 4, 5, 6)
 
 #Code for game 
 while p1 < 50:
-    
+    start = input("Player 1, press 'ENTER' to start your turn:")
 
+    #Generates dice roll for player 1
     p1d1 = random.choice(dice_face)
     p1d2 = random.choice(dice_face)
     p1d3 = random.choice(dice_face)
     print(f"Your dice roll is: {p1d1}, {p1d2}, {p1d3} \n")
 
+    #Checks for tuple out
     if p1d1 == p1d2 and p1d1 == p1d3:
         print("You have Tupled Out! You lose. \n")
         break
     
-
+    #Checks fir fixed dice and option to re-roll
+    if p1d1 == p1d2 or p1d1 == p1d3 or p1d2 == p1d3:
+        while p1d1 == p1d2:
+            roll = input("Do you want to re-roll your 3rd dice? (Y/N) \n")
+            if roll.upper() == 'Y':
+                p1d3 = random.choice(dice_face)
+                print(f"Your dice roll is: {p1d1}, {p1d2}, {p1d3} \n")
+            else:
+                break
 
