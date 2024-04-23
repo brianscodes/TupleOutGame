7ed8935c90ef67b6bad861c3e281add06117e62f
Brian <brians@terpmail.umd.edu>
2024-04-22 21:11:07 -0400

    Setting up the random dice rolling

--- TupleOut.py
+++ TupleOut.py
@@ -6,3 +6,8 @@ p2 = 0
 dice_face = (1, 2, 3, 4, 5, 6)
 
 
+p1d1 = random.choice(dice_face)
+p1d2 = random.choice(dice_face)
+p1d3 = random.choice(dice_face)
+print(f"Your dice roll is: {p1d1}, {p1d2}, {p1d3} \n")
+
